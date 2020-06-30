b8ca474fbe6c3a7ceb0744faebcbd7ae580b3750
Jimy Gabriel Revilla Tellez <jimy_rt_1505@gmail.com>
2020-06-29 21:30:12 -0500

    Se empezo a trabajar con la clase  Destination

--- jimy/travello/views.py
+++ jimy/travello/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 def index(request):
-    return render(request,'index.html',{})
+
+    dest1=Destination()
+    dest1.name = 'PeruCity'
+    dest1.desc = 'La cuna del sillar'
+    return render(request,'index.html',{'dest1':dest1})
