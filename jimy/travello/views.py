8c351560d7aef1d89d918db0b207c1287e5e8e37
Jimy Gabriel Revilla Tellez <jimy_rt_1505@gmail.com>
2020-06-30 21:30:41 -0500

    Ahora se pueden introducir destinos desde el django/admin

--- jimy/travello/views.py
+++ jimy/travello/views.py
@@ -3,27 +3,6 @@ from .models import Destination
 # Create your views here.
 def index(request):
 
-    dest1=Destination()
-    dest1.name = 'PeruCity'
-    dest1.desc = 'La cuna del sillar y el adobo'
-    dest1.price = 500
-    dest1.img='destination_1.jpg'
-    dest1.offer= True
-
-    dest2=Destination()
-    dest2.name = 'Ancash'
-    dest2.desc = 'Aqui tenemos el nevado Huascaran'
-    dest2.price = 500
-    dest2.img='destination_2.jpg'
-    dest2.offer= False
-
-    dest3=Destination()
-    dest3.name = 'Cuzco'
-    dest3.desc = 'El ombligo del mundo, Macchu Picchu'
-    dest3.price = 450
-    dest3.img='destination_3.jpg'
-    dest3.offer= True
-
-    dests=[dest1,dest2,dest3]
+    dests= Destination.objects.all()
 
     return render(request,'index.html',{'dests':dests})
